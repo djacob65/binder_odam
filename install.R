79518063248dff9d92869fe46524a7e13305495d
djacob65@gmail.com <djacob65@gmail.com>
2019-10-15 13:16:04 +0200

    update

--- install.R
+++ install.R
@@ -1,4 +1,4 @@
 install.packages("Rodam")
-install.packages(UpSetR)
-install.packages(data.tree)
-install.packages(dendextend)
+install.packages("UpSetR")
+install.packages("data.tree")
+install.packages("dendextend")
